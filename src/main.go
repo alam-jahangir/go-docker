d5e23f13d857f392740c03af2e67005096f191eb
Md. Jahangir Alam <jahangir033003@gmail.com>
2019-01-20 00:30:47 +0600

    Initial commit of GO & Docker

---
+++ src/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+ "fmt"
+ "net/http"
+ "os"
+)
+
+func main() {
+ var PORT string
+ if PORT = os.Getenv("PORT"); PORT == "" {
+  PORT = "3001"
+ }
+
+ http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "PORT :%s\n, Hello World from path: %s\n", PORT, r.URL.Path)
+ })
+
+ http.ListenAndServe(":" + PORT, nil)
+}
